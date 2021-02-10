9a0040d49e4618bc0fb3b757d5372c2118646bd2
sihyeong <tlgud1013@gamil.com>
2021-02-10 16:32:12 +0900

    blog 4th commit

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,3 +1,9 @@
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
+from bookmark.models import Bookmark
 
-# Create your views here.
+class BookmarkLV(ListView):
+    model = Bookmark
+
+class BookmarkDV(DetailView):
+    model = Bookmark
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+#from bookmark.views import BookmarkLV, BookmarkDV
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('bookmark/', include('bookmark.urls')),
+    path('blog/', include('blog.urls')),
+
+    #path('bookmark/',BookmarkLV.as_View(),name='index'),
+   #path('bookmark/<int:pk>/',BookmarkDV.as_view(),name='detail'),
 ]
