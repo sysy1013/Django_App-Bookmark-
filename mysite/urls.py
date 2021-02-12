e8f901fa6a5022d66fddb031fe189f91580b6ba4
sihyeong <tlgud1013@gamil.com>
2021-02-12 22:34:26 +0900

    blog 5th commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from mysite.views import HomeView
 
 #from bookmark.views import BookmarkLV, BookmarkDV
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',HomeView.as_view(),name='home'),
     path('bookmark/', include('bookmark.urls')),
     path('blog/', include('blog.urls')),
 
